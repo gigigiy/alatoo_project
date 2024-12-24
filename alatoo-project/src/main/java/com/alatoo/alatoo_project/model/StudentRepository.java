a37b69dcedbb8686309858b9aa7e9f6766f80eb4
gigigiy <93245129+gigigiy@users.noreply.github.com>
2024-12-24 13:11:18 +0600

    Delete alatoo-project directory

--- alatoo-project/src/main/java/com/alatoo/alatoo_project/model/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alatoo.alatoo_project.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
-}
